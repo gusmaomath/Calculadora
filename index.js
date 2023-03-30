554ead456cd739c3b11d5e0b6666b5433094ea2c
Gusmão <gusmaomatheusdev@gmail.com>
2023-03-30 17:18:18 -0300

    calculadora-1

---
+++ index.js
@@ -0,0 +1,20 @@
+function insert(num) {
+    var numero = document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = numero + num;
+}
+function clean() {
+    document.getElementById('display').innerHTML = "";
+}
+function back() {
+    var resultado = document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = resultado.substring(0, resultado.length - 1)
+}
+function calc() {
+    var resultado = document.getElementById('display').innerHTML;
+    if (resultado) {
+        document.getElementById('display').innerHTML = eval(resultado);
+    }
+    else {
+        document.getElementById('display').innerHTML = "Error";
+    }
+}
